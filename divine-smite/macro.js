b49c7360ba3fbc419b07cd8f76d8844f19a67da0
Wesley Luk <wesleyluk@gmail.com>
2020-12-24 14:06:04 -0800

    Smite

---
+++ divine-smite/macro.js
@@ -0,0 +1,29 @@
+function doRoll(undead, crit) {
+    let actor = game.actors.get("PKy1niGw3o9BIqPN");
+    let item = actor.getOwnedItem("C2ZGC4CNMumm33pp");
+    BetterRolls.rollItem(item, { event: new KeyboardEvent(""), preset: 0 }, [
+        ["damage", { crit, index: undead ? 1 : 0 }],
+    ]).toMessage();
+}
+let d = new Dialog({
+    title: "Smite Type",
+    buttons: {
+        normal: {
+            label: "Normal",
+            callback: () => doRoll(false, false),
+        },
+        crit: {
+            label: "Critical",
+            callback: () => doRoll(false, true),
+        },
+        undead: {
+            label: "vs Undead",
+            callback: () => doRoll(true, false),
+        },
+        undeadCrit: {
+            label: "Critical vs Undead",
+            callback: () => doRoll(true, true),
+        },
+    },
+});
+d.render(true);
