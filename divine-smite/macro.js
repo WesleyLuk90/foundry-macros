38b58aca23ccf00d41dc31c6e1749735ced20473
Wesley Luk <wesleyluk@gmail.com>
2021-09-04 12:28:22 -0700

    WOrking macro

--- divine-smite/macro.js
+++ divine-smite/macro.js
@@ -2,14 +2,15 @@ function doRoll(undead, crit, level) {
     let actor = game.actors.get("PKy1niGw3o9BIqPN");
     let item = actor.getOwnedItem("C2ZGC4CNMumm33pp");
     let index = 0;
+    console.log(undead, crit, level);
     if(level == 1) {
-        if(undead){
+        if(!undead){
             index = 0;
         } else {
             index = 1;
         }
     } else {
-        if(undead){
+        if(!undead){
             index = 2;
         } else {
             index = 3;
@@ -19,41 +20,52 @@ function doRoll(undead, crit, level) {
         ["damage", { crit, index }],
     ]).toMessage();
 }
-let d = new Dialog({
-    title: "Smite Type",
-    buttons: {
-        normal: {
-            label: "Normal",
-            callback: () => doRoll(false, false, 1),
+function selectHitType(){
+    let d = new Dialog({
+        title: "Fiend/Undead or Other",
+        buttons: {
+            1: {
+                label: "Other",
+                callback: () => selectCrit(false),
+            },
+            2: {
+                label: "Fiend/Undead",
+                callback: () => selectCrit(true),
+            },
         },
-        crit: {
-            label: "Critical",
-            callback: () => doRoll(false, true, 1),
+    });
+    d.render(true);
+}
+function selectCrit(vsFiend){
+    let d = new Dialog({
+        title: "Is Crit?",
+        buttons: {
+            1: {
+                label: "Normal",
+                callback: () => selectLevel(vsFiend, false),
+            },
+            2: {
+                label: "Critical",
+                callback: () => selectLevel(vsFiend, true),
+            },
         },
-        undead: {
-            label: "vs Undead/Fiend",
-            callback: () => doRoll(true, false, 1),
+    });
+    d.render(true);
+}
+function selectLevel(vsFiend, isCrit){
+    let d = new Dialog({
+        title: "Level",
+        buttons: {
+            1: {
+                label: "1",
+                callback: () => doRoll(vsFiend, isCrit, 1),
+            },
+            2: {
+                label: "2",
+                callback: () => doRoll(vsFiend, isCrit, 2),
+            },
         },
-        undeadCrit: {
-            label: "Critical vs Undead/Fiend",
-            callback: () => doRoll(true, true, 1),
-        },
-        normal2: {
-            label: "Normal 2",
-            callback: () => doRoll(false, false, 2),
-        },
-        crit2: {
-            label: "Critical 2",
-            callback: () => doRoll(false, true, 2),
-        },
-        undead2: {
-            label: "2 vs Undead/Fiend",
-            callback: () => doRoll(true, false, 2),
-        },
-        undeadCrit2: {
-            label: "Critical 2 vs Undead/Fiend",
-            callback: () => doRoll(true, true, 2),
-        },
-    },
-});
-d.render(true);
+    });
+    d.render(true);
+}
+selectHitType();
